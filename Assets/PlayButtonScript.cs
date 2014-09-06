f2ed1628d0da190e3c953bc2061a47f91a14f02b
Jessica Wu <jesswu@umich.edu>
2014-09-06 16:45:49 -0400

    Projectile features

---
+++ Assets/PlayButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButtonScript : MonoBehaviour {
+
+	public GUIElement 		button;
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseOver() {
+	//	renderer.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+	}
+}
