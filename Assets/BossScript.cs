0a00a6ae64c134ee85bc2b46b2b85b3316850976
Benjamin <katzbenj@umich.edu>
2014-09-06 20:21:13 -0400

    Made movement of boss work

--- Assets/BossScript.cs
+++ Assets/BossScript.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class BossScript : MonoBehaviour {
 	public float delayBetweenProjectiles = 4f;
-	public KeyCode moveUp;
-	public float		leftAndRightEdge = 50f; //might delete
-	public float		chanceToChangeDirections = 0.1f; //might delete
+	//public KeyCode moveUp;
+	//public float		leftAndRightEdge = 50f; //might delete
+	//public float		chanceToChangeDirections = 0.1f; //might delete
 	public bool			playerHasBeenHit = false; //for collisions
 	public bool 		attackMode = false; //not yet there
-	public float 		bossSpeed = 10f;
+	public float 		bossSpeed = 5f;
 	public Vector3 		pos;//his pos
-	public int 			numProjectiles = 10;
+	public int 			numProjectiles = 3;
 
-	public float		closeDistanceThreshold = 2; //where he should stop near cam
+	public float		closeDistanceThreshold = 6; //where he should stop near cam
 	public float 		farDistanceThreshold = 12;	//where he should stop away from cam
 
 	public GameObject	projectilePrefab;
@@ -20,10 +20,11 @@ public class BossScript : MonoBehaviour {
 
 	public GUIText 		hitsGT;
 	public int			enemyHP = 30;
+	int			initEnemyHealth = 30;
 
-	bool 		bossMoveAway = true;
-	//bool			bossMovesClose = false;
-	int					counter = 0;
+	bool 		bossMoveAway = false;
+	bool		bossMoveClose = true;
+	int			counter = 0;
 	
 
 
@@ -36,28 +37,42 @@ public class BossScript : MonoBehaviour {
 
 		pos = this.transform.position;
 		leftArm = GameObject.Find ("Left hand");
-		Debug.Log(leftArm.transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//basic movement coming towards you may need edits, may prevent backqards movement
-		pos.x += bossSpeed * Time.deltaTime;
-		transform.position = pos;
+	//	Debug.Log (pos.x - Camera.main.transform.position.x);
+	//	Debug.Log (closeDistanceThreshold);
+	//	Debug.Log (!bossMoveAway);
+	  if ((pos.x - Camera.main.transform.position.x) < closeDistanceThreshold && !bossMoveAway) {
+						//he shal stop	
+						//bossSpeed = 0f;
+						if (enemyHP % 5 == 0 && enemyHP != initEnemyHealth) {
+								bossMoveAway = true;
+						}
+						bossMoveClose = false;
+						//same, but in the other direction, but the if clause needs to change to some distance from camera
+		} 
+		else if (bossMoveClose) {
+						//basic movement coming towards you may need edits, may prevent backqards movement
+						//bossSpeed = 10f;
+						pos.x -= bossSpeed * Time.deltaTime;
+						transform.position = pos;
+	}
 		//changing direction
 		//if he gets within distance to camera 
-		if (pos.x - Camera.main.transform.position.x < closeDistanceThreshold) {
 
-			//he shal stop	
-			bossSpeed = 0f;	
-			//same, but in the other direction, but the if clause needs to change to some distance from camera
-		} else if (pos.x - Camera.main.transform.position.x > farDistanceThreshold) {
-			bossSpeed = 0f;	;
-			if (bossMoveAway) {
-				throwProjectiles(numProjectiles);
-			}
-			bossMoveAway = false;
-		}
+		else if (pos.x - Camera.main.transform.position.x > farDistanceThreshold) {
+						//bossSpeed = 0f;	;
+						if (bossMoveAway) {
+								throwProjectiles (numProjectiles);
+						}
+				bossMoveAway = false;
+				} else if (bossMoveAway) {
+						//bossSpeed = -10f;
+						pos.x += bossSpeed * Time.deltaTime;
+						transform.position = pos;
+				} 
 	}
 
 	void throwProjectiles(int num) {
@@ -88,6 +103,7 @@ public class BossScript : MonoBehaviour {
 	void shootProjectile(){
 		if (--counter == 0) {
 			CancelInvoke("shootProjectile");
+			bossMoveClose = true;
 		}
 		GameObject projectile = Instantiate (projectilePrefab) as GameObject;
 		Vector3 projectilePos = leftArm.transform.position;
